c25e4052fff20baec31ed2ca72e6f5ea54d638ef
ASUS <mustofaalisahid@gmail.com>
2019-08-17 23:55:25 +0700

    menambah variable hasil dan membetulkan cetak hasil

--- src/musmus/kalkulator.java
+++ src/musmus/kalkulator.java
@@ -12,8 +12,10 @@ package musmus;
 public class kalkulator {
     int nilaiSatu, nilaiDua;
     void jumlah (){ //deklarasi method instance
+        int hasil = nilaiDua + nilaiSatu;
         System.out.println("Angka pertama \t: " +nilaiSatu);
         System.out.println("Angka kedua \t: " +nilaiDua);
-        System.out.println("\nHasil dari " +nilaiSatu +" + " +nilaiDua +" = " +nilaiDua);
+        System.out.println("\nHasil dari " +nilaiSatu +" + " +nilaiDua +" = " +hasil);
+        System.out.println(hasil);
     }
 }
